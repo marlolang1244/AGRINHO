62dcf0de2bb8d4c315d8e983cb07f9c6471474cf
marlolang1244 <hterraboa@gmail.com>
2024-07-24 07:55:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+function compre() {
+
+
+const width = 800;
+const height = 600;
+const left = (screen.width / 2) - (width / 2);
+const top = (screen.height / 2) - (height / 2);
+window.open('pagamento.html', 'pagamento', `width = $ {width}, heigth = $ {height} , top = $ {top}, left = $ {left}`);}
+
+
+
+
+
